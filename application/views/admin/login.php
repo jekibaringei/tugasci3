c67e89ac5232266790328166fcdf2a2c73fb8243
jekibaringei <jekibaringei@gmail.com>
2022-10-03 12:00:30 +0700

    Perbaikan

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -2,7 +2,7 @@
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">Sign in</p>
 
       <form action="../../index3.html" method="post">
         <div class="input-group mb-3">

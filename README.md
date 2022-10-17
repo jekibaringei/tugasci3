3e5de500ceefabb4ce01fecb309c121538fb44cb
jekibaringei <jekibaringei@gmail.com>
2022-10-17 14:04:46 +0700

    Menambahkan file README.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # FINAL TUGAS CodeIgniter 3 | Penerapan CRUD
 
 Repo untuk tugas matakuliah pemrograman framework
-Bagi Teman yang ingin menggunakan saya persilahkan untuk mendownload atau clone.
+Bagi teman-teman yang ingin menggunakan saya persilahkan untuk mendownload atau clone.
 Ada beberapa konfigurasi yang harus diatur, dibagian config.php dan database.php.
--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -12,7 +12,7 @@
 <?php if($this->session->flashdata('flash')): ?>
   <div class="row mt-3">
     <div class="col md-8">
-      <div class="alert alert-success alert-dismissible fade show" role="alert">Student data has been<?= $this->session->flashdata('flash'); ?>
+      <div class="alert alert-success alert-dismissible fade show" role="alert">Student data has been <?= $this->session->flashdata('flash'); ?>
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     </div>
